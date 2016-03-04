5f71ffed1eab730dddd946646879f44bdbe7a200
illl48 <mruncanny@gmail.com>
2016-03-03 22:59:17 -0800

    fix: add basename example

--- app/App.js
+++ app/App.js
@@ -27,3 +27,31 @@ export class App extends Component {
 		);
 	}
 }
+
+
+//BASENAME example
+/*
+import { Router, Route, useRouterHistory } from 'react-router'
+import { createHistory } from 'history'
+import { syncHistory } from 'redux-simple-router'
+
+import Layout from 'views/Layout'
+import User from 'views/User'
+import About from 'views/About'
+
+const basename = '/test'
+
+const history = useRouterHistory(createHistory)({
+  basename
+})
+
+export const routerMiddleware = syncHistory(history)
+export const routes = (
+  <Router history={history}>
+    <Route path='/' component={Layout}>
+      <Route path='/user' component={User}/>
+      <Route path='/about' component={About}/>
+    </Route>
+  </Router>
+)
+*/
